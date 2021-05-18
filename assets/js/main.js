aa4cc9014a67dde554a78cfedb6c0c769fe20ccf
Santhosh Murthy B K <santhoshmurthybk@gmail.com>
2021-05-18 23:51:49 +0530

    new sections added

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,43 +1,19 @@
-/*===== MENU SHOW Y HIDDEN =====*/
-const navMenu = document.getElementById('nav-menu'),
-    toggleMenu = document.getElementById('nav-toggle'),
-    closeMenu = document.getElementById('nav-close')
+// about section tab
+(() => {
+    const aboutSection = document.querySelector(".about-section"),
+    tabsContainer = document.querySelector(".about-tabs");
 
-// SHOW
-toggleMenu.addEventListener('click', ()=>{
-    navMenu.classList.toggle('show')
-})
+    tabsContainer.addEventListener("click", (event) => {
+        if(event.target.classList.contains("tab-item") && !event.target.classList.contains("active")){
+            const target = event.target.getAttribute("data-target");
 
-// HIDDEN
-closeMenu.addEventListener('click', ()=>{
-    navMenu.classList.remove('show')
-})
+            tabsContainer.querySelector(".active").classList.remove("outer-shadow", "active");
 
-/*===== ACTIVE AND REMOVE MENU =====*/
-const navLink =document.querySelectorAll('.nav__link')
+            event.target.classList.add("active", "outer-shadow");
 
-function linkAction(){
-    navMenu.classList.remove('show')
-}
-navLink.forEach(n=> n.addEventListener('click', linkAction))
+            aboutSection.querySelector(".tab-content.active").classList.remove("active");
 
-/*===== SCROLL SECTIONS ACTIVE LINK =====*/
-const sections = document.querySelectorAll('section[id')
-
-window.addEventListener('scroll', scrollActive)
-
-function scrollActive(){
-    const scrollY = window.pageYOffset
-
-    sections.forEach(current => {
-        const sectionHeight = current.offsetHeight
-        const sectionTop = current.offsetTop - 50
-        sectionId = current.getAttribute('id')
-
-        if(scrollY>sectionTop && scrollY <= sectionTop + sectionHeight){
-            document.querySelector('.nav__menu a[href*='+sectionId+']').classList.add('active')
-        } else {
-            document.querySelector('.nav__menu a[href*='+sectionId+']').classList.remove('active')
+            aboutSection.querySelector(target).classList.add("active");
         }
     })
-}
+})()
